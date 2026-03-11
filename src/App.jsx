ab81428d1960ad61e8a85532487223339dbf08a7
Burak Aydin <aslan.burak_@hotmail.com>
2026-03-11 13:51:32 +0300

    Route And Login Register - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
+import { useState } from "react";
+import Router from "./router/Router";
 
 function App() {
-  return (
-    <div className="App">
-     <h1 class="text-3xl font-bold underline">
-    BURAK AYDIN!
-  </h1>
-    </div>
-  );
+    const [allRoutes, setAllRoutes] = useState([])
+    return <Router allRoutes={allRoutes} />  
 }
 
 export default App;
