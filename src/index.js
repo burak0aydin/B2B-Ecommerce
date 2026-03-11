7d7622777d942ea7aab531e42884d01abb8b570f
Burak Aydin <aslan.burak_@hotmail.com>
2026-03-11 14:20:48 +0300

    Route And Login Register - 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React,{lazy} from 'react';
 import ReactDOM from 'react-dom/client'; 
- 
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const App = lazy(() => import('./App'))
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
